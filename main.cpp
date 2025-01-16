aad3deb60c8b4e24707386d448551d1c516727e0
aliffnajmi05 <aliffnajmi@student.usm.my>
2025-01-16 13:01:02 +0800

    Destinations part is done

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
   |                                   |                   |
   |         TANG JIN SHEN             |      23301943     |
   |     GARETT KHOO BOO ZHENG         |      23301528     |
-  |                                   |                   |
+  |  MUHAMAD ALIFF NAJMI BIN ADENAN   |      23303539     |
   |                                   |                   | 
   |                                   |                   |
   |                                   |                   |
@@ -18,14 +18,29 @@
 #include "LoginRegisterForgot.hpp"
 #include "main_menu.hpp"
 #include "welcome.hpp"
+#include "destination.hpp"
 #include <iostream>
 
 using namespace std;
 
 int main() 
 {
+
     welcomePage();
     LoginRegisterForgot loginregisterforgot;
     mainMenu(loginregisterforgot);
+
+    string destinations[TOTAL_DESTINATION] = {
+        "India", "China", "Indonesia", "Pakistan", "Bangladesh", "Japan", "Philippines", "Vietnam", "Iran",
+        "Turkiye", "Thailand", "Myanmar", "South Korea", "Iraq", "Afghanistan", "Yemen", "Uzbekistan", "Malaysia",
+        "Saudi Arabia", "Nepal", "North Korea","Syria", "Sri Lanka", "Kazakhstan", "Cambodia", "Jordan", "United Arab Emirates",
+         "Tajikistan", "Azerbaijan", "Laos", "Turkmenistan", "Kyrgyzstan","Singapore", "Lebanon", "Palestine", "Oman", "Kuwait", 
+         "Georgia", "Mongolia", "Qatar", "Armenia", "Bahrain", "Timor Leste", "Cyprus", "Bhutan", "Maldives","Brunei"
+    };
+
+    string chosenDestination = selectDestination(destinations);
+    cout << "You have chosen: " << chosenDestination << endl;
+
     return 0;
+
 }
